2ff69d046d6123335e42c1cbd36823ab01c7cd05
tanthanh <tanthanhjan17@gmail.com>
2017-12-04 06:36:48 +0200

    add more initial participants to the list

--- src/static/random-data.js
+++ src/static/random-data.js
@@ -23,5 +23,61 @@ export const randomData = [
     name: 'Chet Baker',
     email: 'jazzy.vibe@mail.com',
     phone: '0981741483'
+  }, {
+    name: 'Francoise Hardy',
+    email: 'chanson@mail.com',
+    phone: '0981333483'
+  }, {
+    name: 'Bill Murray',
+    email: 'rush.more@mail.com',
+    phone: '0909301393'
+  }, {
+    name: 'さとし　こ',
+    email: 'satoshi.kon@mail.com',
+    phone: '0913711819'
+  }, {
+    name: 'Mac DeMarco',
+    email: 'chill.out@mail.com',
+    phone: '0980239091'
+  }, {
+    name: 'Xiao Ming',
+    email: 'xiao.ming@weibo.com',
+    phone: '0981110919'
+  }, {
+    name: '中村',
+    email: 'nakamura@mail.com',
+    phone: '0981341483'
+  }, {
+    name: 'Freud',
+    email: 'psychic.dude@mail.com',
+    phone: '0900013487'
+  }, {
+    name: 'Park Chan-wook',
+    email: 'vengeance@mail.com',
+    phone: '0966693904'
+  }, {
+    name: 'Diego Maradona',
+    email: 'hand.of.god@gmail.com',
+    phone: '0923781993'
+  }, {
+    name: 'Wes Anderson',
+    email: 'moonrise.kingdom@mail.com',
+    phone: '0991301411'
+  }, {
+    name: "You shouldn't be alive",
+    email: 'adventurous.guy@mail.com',
+    phone: '0981741483'
+  }, {
+    name: 'Brian Greene',
+    email: 'elegant.universe@mail.com',
+    phone: '0920309294'
+  }, {
+    name: 'Phan Việt',
+    email: 'nuocmi.nuocmi@mail.com',
+    phone: '0929388924'
+  }, {
+    name: 'Hanoian',
+    email: 'nguyenvu.hoangnguyen@mail.com',
+    phone: '0913411322'
   }
 ]
